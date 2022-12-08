10bf9d771c7c61b0814478cbd960641cc0a3e70d
socosfly <63989353+socosfly@users.noreply.github.com>
2022-12-08 12:04:10 +0800

    Update check.sh

--- Shell/check.sh
+++ Shell/check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bash <(curl -L -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh)) << EOF
+bash <(curl -L -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh) << EOF
 0
 EOF
